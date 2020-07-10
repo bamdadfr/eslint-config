531bd0e6c2a1980a3e7c9a1e7e8237338984e789
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-07-10 08:47:21 +0200

    fix: added jest [force-release]

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "node": true
+        "node": true,
+        "jest": true
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
